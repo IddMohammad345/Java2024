d2d920cfdd7e655476087c9c1da256cd1c794d56
Idd mohammad <mohammadidd345@gmail.com>
2024-02-04 11:05:45 +0530

    4-2-24 commit

---
+++ src/Variables/Empolayee_Details.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class Empolayee_Details {
+    public static void main(String[] args) {
+
+        int Num=420;
+        String name ="Ramu";
+        char gender ='M';
+        double salary = 8792492;
+        long mobile = 9865372997L;
+
+        System.out.println("=============#Emplayee Details#============");
+        System.out.println("ID Number:_ "+Num+"Name:_ "+name+"Salary:_ "+salary+"Mobile Number:_ "+mobile);
+        System.out.println("                                                                                ");
+        System.out.println("================================================================================");
+        System.out.println("                                                                                 ");
+        System.out.println("ID Number    :_ "+Num+"\nName         :_ "+name+"\nSalary       :_ "+salary+"\nMobile Number:_ "+mobile);
+    }
+}
---
+++ src/Variables/Student_Details.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class Student_Details {
+    public static void main(String[] args) {
+
+        int rollNo =45;
+        String name ="Idd mohammad";
+        String course ="java";
+        char gender ='M';
+        double fee  = 53228.64;
+
+
+        System.out.println("----------:printing Student Details:--------------");
+        System.out.println("Roll Number:-" + rollNo);
+        System.out.println("Name       :-" + name);
+        System.out.println("Course     :-" + course);
+        System.out.println("Gender     :-" + gender);
+        System.out.println("Fees       :-" + fee);
+    }
+}
